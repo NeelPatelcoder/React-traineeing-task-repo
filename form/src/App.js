831296c9d530334e4732ad6d01075aa1f5d2fb7e
NeelPatelcoder <neelp.mobio@gmail.com>
2021-07-16 12:33:56 +0530

    form

---
+++ form/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Form from './Components/Form.js';
+
+//import Check from './Check.js';
+
+function App() {
+  
+  return (
+    <div> 
+   <Form />
+    </div>
+  );
+
+}
+
+export default App;
