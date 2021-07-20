5e253c0834aefdb2b4ded97d0e335b52bde5a8cd
NeelPatelcoder <neelp.mobio@gmail.com>
2021-07-20 11:29:40 +0530

    Expenses Display

---
+++ expenses/src/components/AddExpenses.jsx
@@ -0,0 +1,60 @@
+import {FormGroup,FormControl,InputLabel,Input,Button,Typography} from "@material-ui/core"
+import './AddExpenses.css';
+import { useState } from "react";
+import {addData} from '../Services/Api';
+import {useHistory} from 'react-router-dom'
+
+const initialValues = {
+    name: '',
+    item:'',
+    date:'',
+    expense: '',
+}
+
+const AddExpenses = () =>{
+ const [user, setUser] = useState(initialValues)
+ // destructuring 
+ const {name, item, date, expense} = user ;
+
+ //complete
+
+ const history = useHistory();
+
+
+ const onvalueChange= (e) => {
+     setUser(
+         {...user,[e.target.name]:e.target.value}         
+     )
+ }
+
+ const addDatauser = async () =>{
+     await addData(user);
+     history.push('./');
+ }
+
+ return (
+<FormGroup className='container'>
+    <Typography variant='h4'>Add User</Typography>
+    <FormControl>
+        <InputLabel>Name</InputLabel>
+        <Input onChange={ (e) => onvalueChange(e)} name='name' value={name}/>
+    </FormControl>
+    <FormControl>
+        <InputLabel>Item</InputLabel>
+        <Input onChange={ (e) => onvalueChange(e)} name='item' value={item}/>
+    </FormControl>
+    <FormControl>
+        <InputLabel>Date</InputLabel>
+        <Input onChange={ (e) => onvalueChange(e)} name='date' value={date}/>
+    </FormControl>
+    <FormControl>
+        <InputLabel>Expense</InputLabel>
+        <Input onChange={ (e) => onvalueChange(e)} name='expense' value={expense}/>
+    </FormControl>
+    <Button onClick={ () => addDatauser()}>Add Expenses</Button>
+
+</FormGroup>   
+ );
+}
+
+export default AddExpenses;
